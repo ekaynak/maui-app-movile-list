000d00097f88fac1bbf7aef6f98b85e3e307e7e9
erkan kaynak <erkan.kaynak.001@gmail.com>
2023-02-03 18:30:17 +0300

    Main Page

--- MAUI-APP-1/MainPage.xaml.cs
+++ MAUI-APP-1/MainPage.xaml.cs
@@ -4,36 +4,25 @@ namespace MAUI_APP_1;
 
 public partial class MainPage : ContentPage
 {
-	public const double MyFontSize = 28;
-	string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
+
 	public MainPage()
 	{
 		InitializeComponent();
+		NavigationPage.SetHasNavigationBar(this, false);
+		
+		ButtonLogin.Clicked += ButtonLoginClicked;
+	}
 
-		BtnDelete.Clicked += OnDeleteClicked;
-		BtnSave.Clicked += OnSaveClicked;
-		BtnPage2.Clicked += OnBtnPage2Clicked;
-
-		if (File.Exists(_fileName))
+	private void ButtonLoginClicked(object sender, EventArgs e)
+	{
+		if (EntryUsername.Text == "erkan" && entyPassword.Text == "Erkan80")
 		{
-			Editor.Text = File.ReadAllText(_fileName);
+            // DisplayAlert("Login Success", "You can continue to use this application.", "Ok");
+			Navigation.PushModalAsync(new MoviesPage());
+        }
+		else
+		{
+			DisplayAlert("Login Failed", "Invalid username or password.", "Retry");
 		}
-
 	}
-
-	public void OnDeleteClicked(object sender, EventArgs args)
-	{
-		Debug.WriteLine("DELETE");
-	}
-
-	public void OnSaveClicked(object sender, EventArgs args)
-	{
-		Debug.WriteLine("SAVE");
-	}
-
-	public async void OnBtnPage2Clicked(object sender, EventArgs args)
-	{
-		await Navigation.PushAsync(new MoviesPage());
-	}
-
 }
